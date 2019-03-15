33dc735f5279acab9bf592b55cd0ab95ddf0fa62
Rohan Cribbs <rohan.cribbs@thoughtforger.com>
2019-03-14 19:54:41 -0700

    Update PreMiddleware to return a pointer to a context so that the context can be updated. - Updated tests to reflect changes.

--- middleware.go
+++ middleware.go
@@ -11,7 +11,13 @@ import (
 
 // PreMiddleWare is a function that's run against a request before the route's
 // primary function is performed.
-type PreMiddleWare func(context.Context, *Transactor) (*responses.Data, error)
+// Returning a Data struct will short-circut the response and use this response
+// instead of processing the normal handler.
+// Returning a context will make that the new context for future middleware or
+// handlers.
+type PreMiddleWare func(context.Context, *Transactor) (
+    *responses.Data, *context.Context, error,
+)
 
 // PostMiddleWare is a function that's run against a request after the route's
 // primary function is performed.
@@ -22,19 +28,19 @@ type PostMiddleWare func(
 func DefaultEncryptionHeadersMiddleware() PreMiddleWare {
     return func(
         _ context.Context, transactor *Transactor,
-    ) (*responses.Data, error) {
+    ) (*responses.Data, *context.Context, error) {
         err := transactor.AddHeader(
             "Strict-Transport-Security",
             "max-age=63072000; includeSubDomains",
         )
 
         if err != nil {
-            return nil, errors.Wrap(
+            return nil, nil, errors.Wrap(
                 err,
                 "Error while adding default encryption header to response",
             )
         }
 
-        return nil, nil
+        return nil, nil, nil
     }
 }
